6a9197d046659e84e191671cc2a9486514bcae76
james kwok <kinkwanjames@gmail.com>
2022-12-02 11:06:21 -0500

    work on swap offers page

--- src/components/OfferCardList/OfferCardList.jsx
+++ src/components/OfferCardList/OfferCardList.jsx
@@ -1,6 +1,6 @@
-import OfferCard from "../OfferCard/OfferCard";
 import "./OfferCardList.scss";
 import filterIcon from "../../assets/icons/filter.png";
+import OfferCard from "../OfferCard/OfferCard";
 
 const OfferCardList = () => {
   return (
@@ -8,20 +8,20 @@ const OfferCardList = () => {
       <div className="OfferCardList">
         <div className="OfferCardList__container">
           <div className="OfferCardList__text-container">
-            <h1 className="OfferCardList__title">Local Offers</h1>
+            <h1 className="OfferCardList__title">SWAP IN LOCAL OFFERS</h1>
             <p className="OfferCardList__copy">
-              Swap offers to earn more Miles
+              Swap your offers to earn more Miles!
             </p>
           </div>
           <div className="OfferCardList__filter">
-            <img className="OfferCardList__filter-icon" alt="" src={filterIcon} />
+            <img
+              className="OfferCardList__filter-icon"
+              alt=""
+              src={filterIcon}
+            />
           </div>
         </div>
         <div className="OfferCardList__cards">
-          <OfferCard />
-          <OfferCard />
-          <OfferCard />
-          <OfferCard />
           <OfferCard />
         </div>
       </div>
