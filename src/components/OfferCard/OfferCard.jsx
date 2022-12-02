209e8d2804e0703ae014bcf8f57b821834903e43
james kwok <kinkwanjames@gmail.com>
2022-12-02 14:16:55 -0500

    fix images on offer card

--- src/components/OfferCard/OfferCard.jsx
+++ src/components/OfferCard/OfferCard.jsx
@@ -11,17 +11,16 @@ import swapIcon from "../../assets/icons/swap_icon.png";
 import { useNavigate } from "react-router-dom";
 
 const OfferCard = () => {
-
   const navigate = useNavigate();
 
   const handleSwapClick = () => {
-    navigate("/selected")
-    window.scrollTo(0, 0)
+    navigate("/selected");
+    window.scrollTo(0, 0);
   };
 
   const handleDetailsPage = () => {
-    navigate("/offerDetails")
-    window.scrollTo(0, 0)
+    navigate("/offerDetails");
+    window.scrollTo(0, 0);
   };
 
   return (
@@ -73,30 +72,32 @@ const OfferCard = () => {
         </div>
       </div>
       <div className="OfferCard">
-          <div className="OfferCard__banner" onClick={handleDetailsPage}>
-            <span className="OfferCard__banner-text">1000 Bonus Miles</span>
-          </div>
-          <div className="OfferCard__image-container" onClick={handleDetailsPage}>
-            <img
-              className="OfferCard__image"
-              src={americanEagle}
-              alt="sephora-logo"
-            />
-          </div>
-          <div className="OfferCard__text-container" onClick={handleDetailsPage}>
-            <h2 className="OfferCard__title">
-              For every $50 winter collection, online purchase limited
-            </h2>
-            <p className="OfferCard__duration-text">Ends in 2 days</p>
-          </div>
-          <div className="OfferCard__button">
-            <img
-              className="OfferCard__swap-icon"
-              src={swapIcon}
-              alt="swap icon"
-            />
-            <a className="OfferCard__button-text" onClick={handleSwapClick}>Swap</a>
-          </div>
+        <div className="OfferCard__banner" onClick={handleDetailsPage}>
+          <span className="OfferCard__banner-text">1000 Bonus Miles</span>
+        </div>
+        <div className="OfferCard__image-container" onClick={handleDetailsPage}>
+          <img
+            className="OfferCard__image-ae"
+            src={americanEagle}
+            alt="sephora-logo"
+          />
+        </div>
+        <div className="OfferCard__text-container" onClick={handleDetailsPage}>
+          <h2 className="OfferCard__title">
+            For every $50 winter collection, online purchase limited
+          </h2>
+          <p className="OfferCard__duration-text">Ends in 2 days</p>
+        </div>
+        <div className="OfferCard__button">
+          <img
+            className="OfferCard__swap-icon"
+            src={swapIcon}
+            alt="swap icon"
+          />
+          <a className="OfferCard__button-text" onClick={handleSwapClick}>
+            Swap
+          </a>
+        </div>
       </div>
       <div className="OfferCard">
         <div className="OfferCard__banner">
