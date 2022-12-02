3bf4be9b43d6c3af8042cef0aebaea721a94337e
Mason Clarke <maclarkegdci@gmail.com>
2022-12-02 03:58:52 -0500

    Added in nav bar and Your offers component
    Added in nav bar and Your offers component. As well as set up app.js routes for the nav bar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.scss";
 import OfferCardList from "./components/OfferCardList/OfferCardList";
+import YourOffer from "./components/YourOffers/YourOffers.js";
+
+import Header from "./components/Header/Header.js";
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <OfferCardList />
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<YourOffer/>} />
+        <Route path="/swapOffers" element={<OfferCardList/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
